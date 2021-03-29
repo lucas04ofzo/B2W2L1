c796bdca0f2c39c57bb21352aed6af3fce5c9505
Lucas Tigelaar <99061148@mydavinci.nl>
2021-03-29 10:28:47 +0200

    images toegevoegd, buttons goed gezet

--- buttons-template/buttons.js
+++ buttons-template/buttons.js
@@ -1,4 +1,10 @@
-var button1clicked = getElementById("button1")
-button1clicked.onclick = function(){
-	alert("hi");
+function onbutton1clicked(){
+	var text1 = 0
+	document.getElementById("button1").value = text1
+}
+function onbutton2clicked(){
+	var text2 = document.getElementById("button2");
+}
+function onbutton3clicked(){
+	var text3 = document.getElementById("button3");
 }
