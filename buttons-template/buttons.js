1a43f5419ebbf36f58dcaef1514632bef838816f
Lucas Tigelaar <99061148@mydavinci.nl>
2021-04-01 11:16:51 +0200

    images veranderen nu als er geklikt is op een button

--- buttons-template/buttons.js
+++ buttons-template/buttons.js
@@ -1,18 +1,19 @@
-function onbutton1clicked(){
-	var number = 0;
-	number++;
-	var get = document.getElementById("button1");
-	get.innerHTML = number;
+function buttonCounter(imgS,imgL,pId,buttonId){
+	document.getElementById("image").src = imgS;
+	document.getElementById("container").style.backgroundImage = imgL;
+	var remove = document.getElementById(pId);
+	remove.remove();
+	var count = document.createElement("p");
+	count.innerHTML = 1;
+	document.getElementById(buttonId).appendChild(count);
 }
-function onbutton2clicked(){
-	var number = 0;
-	number++;
-	var get = document.getElementById("button2");
-	get.innerHTML = number;
-}
-function onbutton3clicked(){
-	var number = 0;
-	number++;
-	var get = document.getElementById("button3");
-	get.innerHTML = number;
+function buttonClicked(button){
+	var get = document.getElementById(button);
+	if(button == 'button1'){
+		buttonCounter('images/1.jpg', 'url(images/bg1.jpg)', 'p1', 'button1');
+	}else if(button == 'button2'){
+		buttonCounter('images/2.jpg', 'url(images/bg2.jpg)', 'p2', 'button2');
+	}else{
+		buttonCounter('images/3.jpg', 'url(images/bg3.jpg)', 'p3', 'button3');
+	}
 }
