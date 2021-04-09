f7461615e1f003caec506a60264c38acd329a356
Lucas Tigelaar <99061148@mydavinci.nl>
2021-04-09 12:16:31 +0200

    buttons opdracht afgemaakt

--- buttons-template/buttons.js
+++ buttons-template/buttons.js
@@ -1,8 +1,3 @@
-function buttonCounter(imgS, imgL, pId, buttonId){
-	document.getElementById("image").src = imgS;
-	document.getElementById("container").style.backgroundImage = imgL;
-	document.getElementById(pId).innerHTML = 1;
-}
 function buttonClicked(button){
 	var get = document.getElementById(button);
 	if(button == 'button1'){
@@ -13,3 +8,17 @@ function buttonClicked(button){
 		buttonCounter('images/3.jpg', 'url(images/bg3.jpg)', 'p3', 'button3');
 	}
 }
+function buttonCounter(imgS, imgL, pId, buttonId){
+	document.getElementById("image").src = imgS;
+	document.getElementById("container").style.backgroundImage = imgL;
+	if(pId == 'p1'){
+		document.getElementById(pId).innerHTML = number1++
+	}else if(pId == 'p2'){
+		document.getElementById(pId).innerHTML = number2++
+	}else{
+		document.getElementById(pId).innerHTML = number3++
+	}
+}
+var number1 = 1
+var number2 = 1
+var number3 = 1
